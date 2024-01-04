c79db4410982abf4a3799dbfa90883e54715510b
Illia Sakovich <illia.sakovich@gmail.com>
2024-01-04 22:11:23 +0400

    fix colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,34 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     colors: {
-      ...defaultTheme.colors,
+      current: 'currentColor',
+      transparent: 'transparent',
+      black: '#000',
+      white: '#fff',
       pink: {
-        500: '#e11172'
+        50: '#FCDBEB',
+        100: '#FBC3DD',
+        200: '#F794C2',
+        300: '#F365A7',
+        400: '#F0358C',
+        500: '#E11172',
+        600: '#B20D5A',
+        700: '#820A42',
+        800: '#53062A',
+        900: '#240312'
+      },
+      gray: {
+        50: '#fbfbfd',
+        100: '#f5f6fa',
+        150: '#eef0f7',
+        200: '#e7e9f4',
+        300: '#d7daec',
+        400: '#bec3d6',
+        500: '#9797A5',
+        600: '#808091',
+        700: '#3e3e47',
+        800: '#23272c',
+        900: '#17191D'
       },
     },
     extend: {
